cf828b77115b244d9c15306b15d44bc768ff006f
Pete Kinnecom <pete.kinnecom@appfolio.com>
2015-03-27 11:08:36 -0700

    fix require statements

--- lib/app_up.rb
+++ lib/app_up.rb
@@ -1,8 +1,8 @@
-require_relative "app_up/version"
-require_relative 'app_up/path'
-require_relative "app_up/hook_runner"
-require_relative 'app_up/repo'
-require_relative 'app_up/shell_runner'
+require "app_up/version"
+require "app_up/path"
+require "app_up/hook_runner"
+require "app_up/repo"
+require "app_up/shell_runner"
 
 module AppUp
   # Your code goes here...
